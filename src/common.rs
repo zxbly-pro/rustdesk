a5c607c28338af4b84d93e8cd95901021619e0b3
luoju <QWE123qwe!>
2025-09-04 16:54:35 +0800

    修订

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://zxbly.com:21114".to_owned()
 }
 
 #[inline]
@@ -1408,6 +1408,7 @@ pub async fn secure_tcp(conn: &mut Stream, key: &str) -> ResultType<()> {
     // as WebSocket Secure (wss://) already provides transport layer encryption.
     // This doesn't affect the end-to-end encryption between clients,
     // it only avoids redundant encryption between client and server.
+	return Ok(());
     if use_ws() {
         return Ok(());
     }
